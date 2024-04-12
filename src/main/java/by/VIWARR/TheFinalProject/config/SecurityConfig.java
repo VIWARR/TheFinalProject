51ea6b44fd8029ca9b77ad832a142c4717bcf9b9
VIWARR <dm.pavlyuchenko@gmail.com>
2024-04-12 15:37:19 +0300

    authProvider => UserDetailsService

--- src/main/java/by/VIWARR/TheFinalProject/config/SecurityConfig.java
+++ src/main/java/by/VIWARR/TheFinalProject/config/SecurityConfig.java
@@ -1,23 +1,32 @@
 package by.VIWARR.TheFinalProject.config;
 
 import by.VIWARR.TheFinalProject.security.AuthProviderImpl;
+import by.VIWARR.TheFinalProject.services.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
 
-    private final AuthProviderImpl authProvider;
+    private final UserDetailServiceImpl userDetailService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(UserDetailServiceImpl userDetailService) {
+        this.userDetailService = userDetailService;
     }
 
     //Настраивает аутентификацию
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
