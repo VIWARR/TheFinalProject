a6a5a35c6e56cc1816ae439779c49114df623311
VIWARR <dm.pavlyuchenko@gmail.com>
2024-05-09 21:27:21 +0300

    authProvider => UserDetailsService

--- src/main/java/by/VIWARR/TheFinalProject/config/SecurityConfig.java
+++ src/main/java/by/VIWARR/TheFinalProject/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package by.VIWARR.TheFinalProject.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetails userDetails;
+
+
 }
