d22d240262e79c6735eaca77344c73e10b4ee6d3
VIWARR <dm.pavlyuchenko@gmail.com>
2024-05-11 19:37:38 +0300

    ger

--- src/main/java/by/VIWARR/TheFinalProject/controllers/CompanyController.java
+++ src/main/java/by/VIWARR/TheFinalProject/controllers/CompanyController.java
@@ -1,4 +1,28 @@
 package by.VIWARR.TheFinalProject.controllers;
 
+import by.VIWARR.TheFinalProject.security.CompanyDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class CompanyController {
+
+    @GetMapping("/free")
+    public String free() {
+        return "free";
+    }
+
+    @GetMapping("/getCompany")
+    public String getCompany(Model model,
+                             @AuthenticationPrincipal UserDetails userDetails) {
+        CompanyDetails companyDetails = (CompanyDetails) userDetails;
+        model.addAttribute("company", companyDetails.getCompany());
+
+        return "getCompany";
+    }
 }
