ba4e57f561bdf253cab59bb7a8b2c425954ebe8e
VIWARR <dm.pavlyuchenko@gmail.com>
2024-05-12 13:10:29 +0300

    registration

--- src/main/java/by/VIWARR/TheFinalProject/controllers/AuthController.java
+++ src/main/java/by/VIWARR/TheFinalProject/controllers/AuthController.java
@@ -1,7 +1,9 @@
 package by.VIWARR.TheFinalProject.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +13,19 @@ public class AuthController {
     @GetMapping("/loginPage")
     public String loginPage() {
         return "auth/loginPage";
+    }
+
+    @GetMapping("/registrationPage")
+    public String registrationPage() {
+        return "auth/registrationPage";
+    }
+
+    @PostMapping("/performRegistration")
+    public String performRegistration(HttpServletRequest request) {
+        String name = request.getParameter("companyName");
+        String password = request.getParameter("pasword");
+        String description = request.getParameter("description");
+
+
     }
 }
